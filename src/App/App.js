4f3fe05cebc3da04e26c6b5f477c59e86b33de8a
vlad <tarasenkokansasvlad@gmail.com>
2019-11-18 23:23:45 +0200

    Add custom counter hook

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-
+import React from 'react';
+import useCounter from './useCounter';
 import './App.css';
 
 function App() {
-  const [counter, set] = useState(999);
+  const [counter, set] = useCounter(999);
   return (
     <div className="App" onClick={() => set(counter + 1)}>
       {counter}
