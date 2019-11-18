24a2837d7a62fcf3a770ffce438ede9aa59529f4
vlad <tarasenkokansasvlad@gmail.com>
2019-11-18 23:27:41 +0200

    Remove custom counter hook

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import useCounter from './useCounter';
+
 import './App.css';
 
 function App() {
-  const [counter, set] = useCounter(999);
   return (
-    <div className="App" onClick={() => set(counter + 1)}>
-      {counter}
+    <div className="App">
     </div>
   );
 }
