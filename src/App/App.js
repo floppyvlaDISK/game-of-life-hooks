b58fc5e26a1beb50633d14ad49c54cddf2040b72
vlad <tarasenkokansasvlad@gmail.com>
2019-11-20 23:04:49 +0200

    Add reset button

--- src/App/App.js
+++ src/App/App.js
@@ -6,11 +6,16 @@ import AppWrapper from './AppWrapper';
 import useGameOfLife from '../useGameOfLife';
 
 function App() {
-  const [grid, toggleStartPause] = useGameOfLife(7);
+  const [grid, state, toggleState, resetState] = useGameOfLife(5);
   return (
     <AppWrapper>
       <Grid rows={grid.rows} />
-      <Button onClick={toggleStartPause}>Start</Button>
+      <Button onClick={toggleState}>
+        {state === 0 ? 'Start' : 'Pause'}
+      </Button>
+      <Button onClick={resetState} secondary>
+        Reset
+      </Button>
     </AppWrapper>
   );
 }
