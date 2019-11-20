b240a6f1db6ba88e20a8219d148776d2a0e6a8e0
vlad <tarasenkokansasvlad@gmail.com>
2019-11-20 23:05:01 +0200

    Change button styling

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
-  background: tomato;
+  background: ${props => props.secondary ? 'violet' : 'tomato'};
   border-radius: 3px;
   border: none;
   color: white;
   padding: 10px 25px;
   font-size: 20px;
+  margin: 10px;
 `;
 
 export default Button;
