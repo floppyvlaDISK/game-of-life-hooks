c6d5c3897893cd5718397b0d61159afd8db9d9d0
vlad <tarasenkokansasvlad@gmail.com>
2019-11-20 23:55:46 +0200

    Setup game of life class/tests

---
+++ src/GameOfLife.js
@@ -0,0 +1,17 @@
+export default class GameOfLife {
+  static createFromCells(arraysOfCells) {
+    return new this();
+  }
+
+  static createFromSize(aNumber) {
+    return new this();
+  }
+
+  createRows() {
+
+  }
+
+  createCell() {
+
+  }
+}
---
+++ src/GameOfLife.test.js
@@ -0,0 +1,21 @@
+import GameOfLife from './GameOfLife';
+
+it('creates grid with random cells from size', () => {
+  const g = GameOfLife.createFromCells(
+    [
+      [0, 1, 1],
+      [1, 1, 0],
+      [0, 1, 0]
+    ]
+  );
+
+  expect(g.grid).toHaveLength(3);
+  expect(g.grid[0]).toHaveLength(3);
+});
+
+it('creates grid from array of cell values', () => {
+  const g = GameOfLife.createFromSize(3);
+
+  expect(g.grid).toHaveLength(3);
+  expect(g.grid[0]).toHaveLength(3);
+});
