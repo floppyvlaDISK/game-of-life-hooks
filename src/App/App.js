4287724e0d77885319699971f82026512c0bb68c
vlad <tarasenkokansasvlad@gmail.com>
2019-11-20 22:42:10 +0200

    Create button component

--- src/App/App.js
+++ src/App/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './App.css';
 import { Grid } from '../Grid';
+import { Button } from '../Button';
 
 function App() {
   return (
     <div className="App">
       <Grid size={5} />
+      <Button>Start</Button>
     </div>
   );
 }
---
+++ src/Button/Button.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background: tomato;
+  border-radius: 3px;
+  border: none;
+  color: white;
+  padding: 10px 25px;
+  font-size: 20px;
+`;
+
+export default Button;
