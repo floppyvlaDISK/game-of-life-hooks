016d5d8e9dbaa7ddd6a6372d3c71b68d49ee35a0
vlad <tarasenkokansasvlad@gmail.com>
2019-11-21 23:48:03 +0200

    Switch GameOfLife class tests bodies

--- src/GameOfLife.test.js
+++ src/GameOfLife.test.js
@@ -1,6 +1,13 @@
 import GameOfLife from './GameOfLife';
 
 it('creates grid with random cells from size', () => {
+  const g = GameOfLife.createFromSize(3);
+
+  expect(g.grid).toHaveLength(3);
+  expect(g.grid[0]).toHaveLength(3);
+});
+
+it('creates grid from array of cell values', () => {
   const g = GameOfLife.createFromCells(
     [
       [0, 1, 1],
@@ -8,13 +15,6 @@ it('creates grid with random cells from size', () => {
       [0, 1, 0]
     ]
   );
-
-  expect(g.grid).toHaveLength(3);
-  expect(g.grid[0]).toHaveLength(3);
-});
-
-it('creates grid from array of cell values', () => {
-  const g = GameOfLife.createFromSize(3);
 
   expect(g.grid).toHaveLength(3);
   expect(g.grid[0]).toHaveLength(3);
