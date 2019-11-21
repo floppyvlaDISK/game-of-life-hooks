8fad6b1f1b2b4e206d1ecce326b75eb74c47f015
vlad <tarasenkokansasvlad@gmail.com>
2019-11-22 00:02:03 +0200

    Implement static methods for creating GameOfLife instance(draft)

--- src/GameOfLife.js
+++ src/GameOfLife.js
@@ -4,22 +4,48 @@ export default class GameOfLife {
   }
 
   static createFromCells(arraysOfCells) {
-    return new this([]);
+    return new this(GameOfLife._createGridFromCells(arraysOfCells));
   }
 
-  static createFromSize(aNumber) {
-    return new this([]);
+  static createFromSize(size) {
+    return new this(GameOfLife._createGridFromSize(size));
+  }
+
+  static _createGridFromCells(arraysOfCells) {
+    let result = [];
+    for (let row = 0; row < arraysOfCells.length; row++) {
+      let cellsForRow = [];
+      for (let col = 0; col < arraysOfCells[row].length; col++) {
+        cellsForRow.push(
+          new Cell(arraysOfCells[row][col])
+        );
+      }
+      result.push(cellsForRow);
+    }
+    return result;
+  }
+
+  static _createGridFromSize(size) {
+    let result = [];
+    for (let row = 0; row < size; row++) {
+      let cellsForRow = [];
+      for (let col = 0; col < size; col++) {
+        cellsForRow.push(
+          new Cell(Math.round(Math.random())),
+        );
+      }
+      result.push(cellsForRow);
+    }
+    return result;
   }
 
   get grid() {
     return this._grid;
   }
+}
 
-  createRows() {
-
-  }
-
-  createCell() {
-
+class Cell {
+  constructor(state) {
+    this._state = state || 0;
   }
 }
