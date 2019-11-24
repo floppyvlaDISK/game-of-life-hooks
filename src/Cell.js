3e3201bd991b6da4768dfeffc27ea1cfac25f5d1
vlad <tarasenkokansasvlad@gmail.com>
2019-11-24 22:52:22 +0200

    Move Cell to a separate component

---
+++ src/Cell.js
@@ -0,0 +1,15 @@
+import { CELL_STATES } from "./Grid/CONST";
+
+export default class Cell {
+  constructor(state) {
+    this._state = state || 0;
+  }
+
+  static getRandomState() {
+    return Math.random() > 0.5 ? CELL_STATES.dead : CELL_STATES.alive;
+  }
+
+  get state() {
+    return this._state;
+  }
+}
--- src/GameOfLife.js
+++ src/GameOfLife.js
@@ -1,4 +1,4 @@
-import { CELL_STATES } from "./Grid/CONST";
+import Cell from './Cell';
 
 export default class GameOfLife {
   constructor(grid) {
@@ -43,19 +43,5 @@ export default class GameOfLife {
 
   get grid() {
     return this._grid;
-  }
-}
-
-class Cell {
-  constructor(state) {
-    this._state = state || 0;
-  }
-
-  static getRandomState() {
-    return Math.random() > 0.5 ? CELL_STATES.dead : CELL_STATES.alive;
-  }
-
-  get state() {
-    return this._state;
   }
 }
