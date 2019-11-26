8a56e5d77396f8e90323d3afece16e4bdb5f14dc
vlad <tarasenkokansasvlad@gmail.com>
2019-11-26 23:14:28 +0200

    Fix typo in NumberRange tests

--- src/utils/NumberRange.test.js
+++ src/utils/NumberRange.test.js
@@ -3,13 +3,13 @@ import NumberRange from './NumberRange';
 it('contains() lower boundry', () => {
   const r = new NumberRange(0, 3);
 
-  expect(r.contains(0)).to.equal(true);
-  expect(r.contains(-1)).to.equal(false);
+  expect(r.contains(0)).toBe(true);
+  expect(r.contains(-1)).toBe(false);
 });
 
 it('contains() higher boundry', () => {
   const r = new NumberRange(0, 3);
 
-  expect(r.contains(3)).to.equal(true);
-  expect(r.contains(4)).to.equal(false);
+  expect(r.contains(3)).toBe(true);
+  expect(r.contains(4)).toBe(false);
 });
